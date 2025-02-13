364bc952bacd8e3c595e4d0f73d648550b17c09f
wahidman <wahidman948@gmail.com>
2025-02-13 18:27:30 +0700

    Update server.js

--- server.js
+++ server.js
@@ -12,16 +12,15 @@ app.use(cors());
 app.use(bodyParser.json());
 
 // Koneksi ke PostgreSQL
-const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
-    host: process.env.DB_HOST,
-    dialect: 'postgres',
-    port: process.env.DB_PORT,
-    dialectOptions: {
-        ssl: {
-            require: true,
-            rejectUnauthorized: false // Untuk menghindari error SSL
-        }
-    }
+const sequelize = new Sequelize(process.env.DATABASE_URL, {
+  dialect: 'postgres',
+  protocol: 'postgres',
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false,
+    },
+  },
 });
 
 
